c59128a63be2c80eb2158a6b3cab6ac3c021a41c
“Zhu Chen” <znchen720@gmail.com>
2017-03-23 19:05:47 -0400

    Testing for deployment

---
+++ assets/final-project.js
@@ -0,0 +1,295 @@
+"use strict";
+
+
+
+define('final-project/adapters/application', ['exports', 'final-project/config/environment', 'active-model-adapter'], function (exports, _finalProjectConfigEnvironment, _activeModelAdapter) {
+  exports['default'] = _activeModelAdapter['default'].extend({
+    host: _finalProjectConfigEnvironment['default'].apiHost
+  });
+});
+define('final-project/app', ['exports', 'ember', 'final-project/resolver', 'ember-load-initializers', 'final-project/config/environment'], function (exports, _ember, _finalProjectResolver, _emberLoadInitializers, _finalProjectConfigEnvironment) {
+
+  var App = undefined;
+
+  _ember['default'].MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember['default'].Application.extend({
+    modulePrefix: _finalProjectConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _finalProjectConfigEnvironment['default'].podModulePrefix,
+    Resolver: _finalProjectResolver['default']
+  });
+
+  (0, _emberLoadInitializers['default'])(App, _finalProjectConfigEnvironment['default'].modulePrefix);
+
+  exports['default'] = App;
+});
+define('final-project/controllers/array', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('final-project/controllers/object', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Controller;
+});
+define('final-project/helpers/app-version', ['exports', 'ember', 'final-project/config/environment', 'ember-cli-app-version/utils/regexp'], function (exports, _ember, _finalProjectConfigEnvironment, _emberCliAppVersionUtilsRegexp) {
+  exports.appVersion = appVersion;
+  var version = _finalProjectConfigEnvironment['default'].APP.version;
+
+  function appVersion(_) {
+    var hash = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+    if (hash.hideSha) {
+      return version.match(_emberCliAppVersionUtilsRegexp.versionRegExp)[0];
+    }
+
+    if (hash.hideVersion) {
+      return version.match(_emberCliAppVersionUtilsRegexp.shaRegExp)[0];
+    }
+
+    return version;
+  }
+
+  exports['default'] = _ember['default'].Helper.helper(appVersion);
+});
+define('final-project/helpers/pluralize', ['exports', 'ember-inflector/lib/helpers/pluralize'], function (exports, _emberInflectorLibHelpersPluralize) {
+  exports['default'] = _emberInflectorLibHelpersPluralize['default'];
+});
+define('final-project/helpers/singularize', ['exports', 'ember-inflector/lib/helpers/singularize'], function (exports, _emberInflectorLibHelpersSingularize) {
+  exports['default'] = _emberInflectorLibHelpersSingularize['default'];
+});
+define("final-project/initializers/active-model-adapter", ["exports", "active-model-adapter", "active-model-adapter/active-model-serializer"], function (exports, _activeModelAdapter, _activeModelAdapterActiveModelSerializer) {
+  exports["default"] = {
+    name: 'active-model-adapter',
+    initialize: function initialize() {
+      var application = arguments[1] || arguments[0];
+      application.register('adapter:-active-model', _activeModelAdapter["default"]);
+      application.register('serializer:-active-model', _activeModelAdapterActiveModelSerializer["default"]);
+    }
+  };
+});
+define('final-project/initializers/app-version', ['exports', 'ember-cli-app-version/initializer-factory', 'final-project/config/environment'], function (exports, _emberCliAppVersionInitializerFactory, _finalProjectConfigEnvironment) {
+  var _config$APP = _finalProjectConfigEnvironment['default'].APP;
+  var name = _config$APP.name;
+  var version = _config$APP.version;
+  exports['default'] = {
+    name: 'App Version',
+    initialize: (0, _emberCliAppVersionInitializerFactory['default'])(name, version)
+  };
+});
+define('final-project/initializers/container-debug-adapter', ['exports', 'ember-resolver/container-debug-adapter'], function (exports, _emberResolverContainerDebugAdapter) {
+  exports['default'] = {
+    name: 'container-debug-adapter',
+
+    initialize: function initialize() {
+      var app = arguments[1] || arguments[0];
+
+      app.register('container-debug-adapter:main', _emberResolverContainerDebugAdapter['default']);
+      app.inject('container-debug-adapter:main', 'namespace', 'application:main');
+    }
+  };
+});
+define('final-project/initializers/data-adapter', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `data-adapter` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'data-adapter',
+    before: 'store',
+    initialize: function initialize() {}
+  };
+});
+define('final-project/initializers/ember-data', ['exports', 'ember-data/setup-container', 'ember-data/-private/core'], function (exports, _emberDataSetupContainer, _emberDataPrivateCore) {
+
+  /*
+  
+    This code initializes Ember-Data onto an Ember application.
+  
+    If an Ember.js developer defines a subclass of DS.Store on their application,
+    as `App.StoreService` (or via a module system that resolves to `service:store`)
+    this code will automatically instantiate it and make it available on the
+    router.
+  
+    Additionally, after an application's controllers have been injected, they will
+    each have the store made available to them.
+  
+    For example, imagine an Ember.js application with the following classes:
+  
+    App.StoreService = DS.Store.extend({
+      adapter: 'custom'
+    });
+  
+    App.PostsController = Ember.Controller.extend({
+      // ...
+    });
+  
+    When the application is initialized, `App.ApplicationStore` will automatically be
+    instantiated, and the instance of `App.PostsController` will have its `store`
+    property set to that instance.
+  
+    Note that this code will only be run if the `ember-application` package is
+    loaded. If Ember Data is being used in an environment other than a
+    typical application (e.g., node.js where only `ember-runtime` is available),
+    this code will be ignored.
+  */
+
+  exports['default'] = {
+    name: 'ember-data',
+    initialize: _emberDataSetupContainer['default']
+  };
+});
+define('final-project/initializers/export-application-global', ['exports', 'ember', 'final-project/config/environment'], function (exports, _ember, _finalProjectConfigEnvironment) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_finalProjectConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var theGlobal;
+      if (typeof window !== 'undefined') {
+        theGlobal = window;
+      } else if (typeof global !== 'undefined') {
+        theGlobal = global;
+      } else if (typeof self !== 'undefined') {
+        theGlobal = self;
+      } else {
+        // no reasonable global, just bail
+        return;
+      }
+
+      var value = _finalProjectConfigEnvironment['default'].exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember['default'].String.classify(_finalProjectConfigEnvironment['default'].modulePrefix);
+      }
+
+      if (!theGlobal[globalName]) {
+        theGlobal[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete theGlobal[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports['default'] = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('final-project/initializers/injectStore', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `injectStore` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'injectStore',
+    before: 'store',
+    initialize: function initialize() {}
+  };
+});
+define('final-project/initializers/store', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `store` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'store',
+    after: 'ember-data',
+    initialize: function initialize() {}
+  };
+});
+define('final-project/initializers/transforms', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `transforms` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'transforms',
+    before: 'store',
+    initialize: function initialize() {}
+  };
+});
+define("final-project/instance-initializers/ember-data", ["exports", "ember-data/-private/instance-initializers/initialize-store-service"], function (exports, _emberDataPrivateInstanceInitializersInitializeStoreService) {
+  exports["default"] = {
+    name: "ember-data",
+    initialize: _emberDataPrivateInstanceInitializersInitializeStoreService["default"]
+  };
+});
+define('final-project/resolver', ['exports', 'ember-resolver'], function (exports, _emberResolver) {
+  exports['default'] = _emberResolver['default'];
+});
+define('final-project/router', ['exports', 'ember', 'final-project/config/environment'], function (exports, _ember, _finalProjectConfigEnvironment) {
+
+  var Router = _ember['default'].Router.extend({
+    location: _finalProjectConfigEnvironment['default'].locationType,
+    rootURL: _finalProjectConfigEnvironment['default'].rootURL
+  });
+
+  Router.map(function () {});
+
+  exports['default'] = Router;
+});
+define('final-project/serializers/application', ['exports', 'active-model-adapter'], function (exports, _activeModelAdapter) {
+  exports['default'] = _activeModelAdapter.ActiveModelSerializer.extend({});
+});
+define('final-project/services/ajax', ['exports', 'ember-ajax/services/ajax'], function (exports, _emberAjaxServicesAjax) {
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function get() {
+      return _emberAjaxServicesAjax['default'];
+    }
+  });
+});
+define('final-project/services/auth', ['exports', 'ember'], function (exports, _ember) {
+  exports['default'] = _ember['default'].Service.extend({});
+});
+define("final-project/templates/application", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template({ "id": "ZKB13zRG", "block": "{\"statements\":[[\"open-element\",\"h1\",[]],[\"flush-element\"],[\"text\",\" Welcome to Ember! \"],[\"close-element\"],[\"text\",\"\\n\\n\"],[\"append\",[\"unknown\",[\"outlet\"]],false],[\"text\",\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"blocks\":[],\"hasPartials\":false}", "meta": { "moduleName": "final-project/templates/application.hbs" } });
+});
+
+
+define('final-project/config/environment', ['ember'], function(Ember) {
+  var prefix = 'final-project';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+if (!runningTests) {
+  require("final-project/app")["default"].create({"name":"final-project","version":"0.0.1+d17d53a0"});
+}
+//# sourceMappingURL=final-project.map
